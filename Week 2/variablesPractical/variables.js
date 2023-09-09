3bf89c0153bfaeaa7b83903c08c3b8a6c17d0381
Aubrey <120981800+grav0165@users.noreply.github.com>
2023-09-08 20:31:09 -0500

    Delete Week 2 directory

--- Week 2/variablesPractical/variables.js
+++
@@ -1,7 +0,0 @@
-console.log( 'js' );
-let testVar = 9;
-console.log(testVar);
-console.log('the value of testVar is', testVar);
-
-testVar = 7654456;
-console.log('the value of testVar is', testVar);
